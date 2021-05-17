f96dd92daff13b11c4c4156ce57dc9ca295cb995
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-05-18 00:53:06 +0100

    get users investigating syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import API from './utils/API';
+import EmpDir from './components/EmpDir';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    usersFiltered: [],
+    order: "ascend"
+  };
+
+
+componentDidMount() {
+  this.getUsers();
 }
+
+getUsers = async () => {
+  try {
+    const response = await API.getUsers();
+    console.group(response.data.results);
+    const empDB = response.data.results.map(x => ({
+      img: x.picture.medium,
+      lastName: x.name.last,
+      firstName: x.name.first,
+      phone: x.phone,
+      cell: x.cell,
+      email: x.email
+    }));
+    this.setState({ users: empDB, usersFiltered: empDB });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 export default App;
