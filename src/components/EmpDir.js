993c82ac7e8be0a11ee8dc0a97f076068f96a907
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-05-19 00:19:51 +0100

    amended empdir.js - wrong fields in phone, cell, email - td corrected

--- src/components/EmpDir.js
+++ src/components/EmpDir.js
@@ -6,28 +6,34 @@ function EmpDir(props) {
             <tr>
                 <td className="align-middle"><img src={props.employee.img} alt="Employee Picture" /></td>
                 <td className="text-center align-middle">{props.employee.firstName + " " + props.employee.lastName}</td>
-                <th data-name="firstName" onClick={props.sortTable}>Name 🔻</th>
-                <th data-name="phone" onClick={props.sortTable}>Phone</th>
-                <th data-name="cell" onClick={props.sortTable}>Cell</th>
-                <th data-name="email" onClick={props.sortTable}>Email</th>
+                <td className="text-center align-middle">{props.employee.phone}</td>
+                <td className="text-center align-middle">{props.employee.cell}</td>
+                <td className="text-center align-middle">{props.employee.email}</td>
             </tr>
         );
     };
 
 
-return (
-    <div className="wrapper">
-        <table className="table table-striped table-hover">
-            <thead className="thead-dark">
-                <th data-name="img" onClick={props.sortTable}></th>
-                <th data-name="firstName" onClick={props.sortTable}>Name 🔻</th>
-                <th data-name="phone" onClick={props.sortTable}>Phone</th>
-                <th data-name="cell" onClick={props.sortTable}>Cell</th>
-                <th data-name="email" onClick={props.sortTable}>Email</th>
-            </thead>
-        </table>
-    </div>
-);
+    return (
+        <div className="wrapper">
+            <table className="table table-striped table-hover">
+                <thead className="thead-dark">
+                    <tr>
+                        <th data-name="img" onClick={props.sortTable}></th>
+                        <th data-name="firstName" onClick={props.sortTable}>Name 🔻</th>
+                        <th data-name="phone" onClick={props.sortTable}>Phone</th>
+                        <th data-name="cell" onClick={props.sortTable}>Cell</th>
+                        <th data-name="email" onClick={props.sortTable}>Email</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        {props.employees.map((x, i) => (
+                            <Row employee={x} key={i + "-empRow"} />
+                        ))}
+                    </tbody>
+            </table>
+        </div>
+    );
 
 }
 
