fbde8bd6747543b20dd18fc347b88b6179fd39ea
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-05-18 01:07:47 +0100

    Successful render of headings of table

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import API from './utils/API';
 import EmpDir from './components/EmpDir';
 import React from 'react';
 
+
 class App extends React.Component {
   state = {
     users: [],
@@ -11,29 +12,54 @@ class App extends React.Component {
     order: "ascend"
   };
 
-
-componentDidMount() {
-  this.getUsers();
-}
-
-getUsers = async () => {
-  try {
-    const response = await API.getUsers();
-    console.group(response.data.results);
-    const empDB = response.data.results.map(x => ({
-      img: x.picture.medium,
-      lastName: x.name.last,
-      firstName: x.name.first,
-      phone: x.phone,
-      cell: x.cell,
-      email: x.email
-    }));
-    this.setState({ users: empDB, usersFiltered: empDB });
-
-  } catch (error) {
-    console.log(error);
+  componentDidMount() {
+    this.getUsers();
   }
-}
+
+  getUsers = async () => {
+    try {
+      const response = await API.getUsers();
+      console.group(response.data.results);
+
+      const empDB = response.data.results.map(x => ({
+        img: x.picture.medium,
+        lastName: x.name.last,
+        firstName: x.name.first,
+        phone: x.phone,
+        cell: x.cell,
+        email: x.email
+      }));
+
+      this.setState({ users: empDB, usersFiltered: empDB });
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleInput = (val) => {
+    this.setState({
+      users: this.state.usersFiltered.filter((x) => x.firstName.includes(val)),
+    });
+  };
 
 
-export default App;
+
+    render() {
+      return (
+        <div className="text-center mb-4">
+          <h1 className="text-center mb-4 searchbar">Employee Directory</h1>
+
+          <label className="text-center mb-4" htmlFor="text">
+            <input className="text-center mb-4" placeholder="Search" type="text"
+              onChange={(e) => this.handleInput(e.target.value)} /></label>
+
+          <EmpDir employees={this.state.users} sortTable={this.sortTable} />
+        </div>
+      );
+    }
+
+  
+};
+
+  export default App;
