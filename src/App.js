03b022bfe3f144e603ec3d526d0ad13d686eac1d
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-05-19 02:12:24 +0100

    updated App.js to include sort on first

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     order: "ascend"
   };
 
+  // When this component mounts, search "
   componentDidMount() {
     this.getUsers();
   }
@@ -44,22 +45,73 @@ class App extends React.Component {
   };
 
 
+  // creating sort
+  employeeSorted = () => {
+    const sortedUsers = this.state.usersFiltered;
 
-    render() {
-      return (
-        <div className="text-center mb-4">
-          <h1 className="text-center mb-4 searchbar">Employee Directory</h1>
+    sortedUsers.sort(function (a, b) {
+      console.log(a.first, "a.value", b.first, "b.value");
+      var empA = a.name.first.toLowerCase();
+      var empB = a.name.first.toLowerCase();
 
-          <label className="text-center mb-4" htmlFor="text">
-            <input className="text-center mb-4" placeholder="Search" type="text"
-              onChange={(e) => this.handleInput(e.target.value)} /></label>
+      if (empA < empB) {
+        return -1;
+      }
+      if (empA > empB) {
+        return 1;
+      }
+      return 0;
+    });
 
-          <EmpDir employees={this.state.users} sortTable={this.sortTable} />
-        </div>
-      );
-    }
+    this.setState = {
+      usersFiltered: sortedUsers
+    };
 
-  
-};
+    this.setState({
+      usersFiltered: this.state.users.sort((a, b) => {
+        console.log(a.first, "a.value", b.first, "b.value");
+        var empA = a.name.first.toLowerCase();
+        var empB = a.name.first.toLowerCase();
 
-  export default App;
+        if (empA < empB) {
+          return 1;
+        }
+        if (empA > empB) {
+          return -1;
+        }
+        return 0;
+      }),
+    });
+
+    return this.setState({
+      order: "ascend"
+    });
+  };
+
+
+  sortTable = (e) => {
+    const key = e.target.getAttribute("data-name");
+
+    this.setState({
+      users: this.state.users.sort((a, b) => (a[key] > b[key] ? 1 : -1)),
+    });
+  };
+
+
+  render() {
+    return (
+      <div className="text-center mb-4">
+        <h1 className="text-center mb-4 searchbar">Employee Directory</h1>
+
+        <label className="text-center mb-4" htmlFor="text">
+          <input className="text-center mb-4" placeholder="Search" type="text"
+            onChange={(e) => this.handleInput(e.target.value)} /></label>
+
+        <EmpDir employees={this.state.users} sortTable={this.sortTable} />
+      </div>
+    );
+
+  }
+}
+
+export default App;
