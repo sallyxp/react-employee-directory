95d50bf5e6582b59d289f3ad1aa155253ed1d2e3
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-05-26 01:34:23 +0100

    modified Main index.js

---
+++ src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import React from "react";
+import EmployeeCard from "../EmployeeCard";
+import "./style.css";
+
+function Main() {
+    return (
+        <div className="main">
+            <EmployeeCard />
+        </div>
+    )
+}
+
+export default Main;
